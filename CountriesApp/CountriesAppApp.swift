1033f56bc5ab767e46d4882fd79250bbc0f1e22a
ahmednasr <ahmed.nasr.saieed@gmail.com>
2026-05-14 20:27:24 +0300

    Initial Commit

---
+++ CountriesApp/CountriesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountriesAppApp.swift
+//  CountriesApp
+//
+//  Created by Ahmed Nasr on 14/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct CountriesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
